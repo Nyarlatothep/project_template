17a25799248a1f0a117408219dd4795c62cc92be
Nyarlatothep <nyarlathotep.prog@gmail.com>
2017-07-07 13:42:40 -0500

    Add space before comments

--- src/template_files.cpp
+++ src/template_files.cpp
@@ -8,12 +8,12 @@ std::string CMakeListsSrcFile() {
 std::string CMakeListsRootFile(const std::string name) {
   return R"delim(cmake_minimum_required(VERSION 3.8)
 
-#Using c++17 for all personal projects
+# Using c++17 for all personal projects
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
-#Output compile_commands.json
+# Output compile_commands.json
 set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 
 # Add warnings
@@ -23,20 +23,20 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything")
 endif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 
-#Change project name here
+# Change project name here
 set(PROJECT_NAME )delim" +
          name + R"delim()
-#Where to build the files
+# Where to build the files
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
 
 project(${PROJECT_NAME})
 
-#Uncomment for most used Boost libraries
+# Uncomment for most used Boost libraries
 #find_package(Boost REQUIRED COMPONENTS system filesystem program_options)
 
 include_directories(include)
 
-#Assumes a library called project_library is defined there
+# Assumes a library called project_library is defined there
 add_subdirectory(src)
 
 add_executable(main src/main.cpp)
