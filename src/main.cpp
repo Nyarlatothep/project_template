576759b540dec18fd7ee3503a719788e7fa99181
Nyarlatothep <nyarlathotep.prog@gmail.com>
2017-07-18 11:12:05 -0500

    Switch from boost to llvm for command line parsing
    Command Line parsing by llvm seems easier and more readable.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
-#include <boost/program_options.hpp>
 #define BOOST_FILESYSTEM_NO_DEPRECATED
 
-#include "range/v3/action.hpp"
-
+#include <llvm/Support/CommandLine.h>
+#include <range/v3/action.hpp>
 #include <string>
 
 #include "cmake_execution.hpp"
 #include "default_project.hpp"
 
-boost::program_options::variables_map setup_command_line(int argc, char* arv[]);
-std::vector<std::string> get_project_names(
-    boost::program_options::variables_map vm);
-
 int main(int argc, char* argv[]) {
   namespace fs = boost::filesystem;
+  using namespace llvm;
   using namespace ranges;
 
-  auto variable_map = setup_command_line(argc, argv);
-  auto project_names = get_project_names(variable_map);
-
   auto cwd = fs::current_path();
+
+  // Command Line Parsing
+  cl::opt<bool> RunCMake("cmake", cl::desc("Run CMake from build directory"));
+  cl::list<std::string> InputProjectNames(
+      cl::Positional, cl::desc("<Project Names>"), cl::Required);
+  cl::ParseCommandLineOptions(argc, argv);
+  std::vector<std::string> project_names = InputProjectNames;
 
   auto invalid_project_name = [&cwd](const std::string name) {
     return !fs::portable_directory_name(name) || fs::exists(cwd / name);
@@ -36,37 +36,10 @@ int main(int argc, char* argv[]) {
     Folder project = default_project(name);
     project.write_to(cwd);
 
-    run_cmake(project_path / "build");
+    if (RunCMake) {
+      run_cmake(project_path / "build");
+    }
   }
 
   return 0;
-}
-
-boost::program_options::variables_map setup_command_line(int argc,
-                                                         char* argv[]) {
-  namespace po = boost::program_options;
-  po::options_description desc{"Allowed options"};
-  desc.add_options()("help", "produce help message")(
-      "project-name", po::value<std::vector<std::string>>(), "project name");
-
-  po::positional_options_description no_prefix_default;
-  no_prefix_default.add("project-name", -1);
-
-  po::variables_map vm;
-  po::store(po::command_line_parser(argc, argv)
-                .options(desc)
-                .positional(no_prefix_default)
-                .run(),
-            vm);
-  po::notify(vm);
-  return vm;
-}
-
-std::vector<std::string> get_project_names(
-    boost::program_options::variables_map vm) {
-  if (vm.count("project-name")) {
-    return vm["project-name"].as<std::vector<std::string>>();
-  } else {
-    return {};
-  }
 }
